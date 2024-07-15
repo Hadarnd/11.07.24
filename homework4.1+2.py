595847cdf6077c275b91e1ead0cd9cf51f13ea74
Hadar <hadarnd8@gmail.com>
2024-07-15 15:12:05 +0300

    Add files via upload

---
+++ homework4.1+2.py
@@ -0,0 +1,56 @@
+#start
+
+a = 2
+b = 2
+
+if a == b:
+    print(f"was true for {a} {b}")
+else:
+    print(f"was fulse for {a} {b}")
+
+a = 0
+b = 0
+c = 0
+d = 0
+
+print(f"was falsse for {a} {b} {c} {d}")
+
+
+#_________________________________________________________
+if 4<9:
+    print("true")
+
+if (2*3+4)+(7+7):
+    print("true")
+if 18+18:
+    print("true")
+if 10 == 10:
+    print("true")
+if 10 == 10 and 20 == 30:
+    print("true")
+if not 3:
+    print("false")
+if 3:
+    print("true")
+if (33>20) and (2<10) and 10:
+    print("true")
+if true and true:
+    print("true")
+if true or false:
+if false or true:
+if (not 10) and(10):
+    print("true")
+if 2 == 2 or 3 == 3:
+    print("true")
+if 40 == 30 1>4:
+    print("false")
+if 13 >= 3 or 47 >= 5:
+    print("true")
+
+
+
+
+
+
+
+
---
+++ homework4.3.py
@@ -0,0 +1,7 @@
+#start
+
+num: float = float(input("Please enter current temperature:"))
+
+print("hot" if num > 30 else "normal")
+
+#end
