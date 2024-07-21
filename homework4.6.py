55c64b74b9b8197dc5f17359d9b289f9dcd7a4ad
Hadar <hadarnd8@gmail.com>
2024-07-21 15:52:03 +0300

    Add files via upload

---
+++ homework4.6.py
@@ -0,0 +1,8 @@
+x: str = str("good")
+y: str = str("morning")
+
+while True:
+    x = str(input("what is the opposite of bad?"))
+    y = str(input("what part of the day is in 8am?"))
+    print(f"{x} {y}")
+    break
